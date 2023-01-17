552d3152f85fe5608af2e31db6bba1c07672f472
erashu212 <ashu.singh212@gmail.com>
2023-01-17 12:53:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# questions-answers-coding
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-fk8yjc)
---
+++ index.js
@@ -0,0 +1,28 @@
+// run `node index.js` in the terminal
+
+const isEven = (num) => num % 2 === 0;
+
+const generatePalindrome = (str) => {
+  const len = str.length;
+  if (len % 2 === 0) return null;
+  const charsMap = str.split('').reduce((acc, ch) => {
+    return {
+      ...acc,
+      [ch]: (acc[ch] || 0) + 1,
+    };
+  }, {});
+  console.log('charsMap', charsMap);
+  let results = ['', '', ''];
+  for (const [ch, count] of Object.entries(charsMap)) {
+    if (!isEven(count)) {
+      if (results[0] !== '') return null;
+      results[0] = ch.repeat(count);
+    } else {
+      results[1] += ch.repeat(count / 2);
+      results[2] += ch.repeat(count / 2);
+    }
+  }
+  console.log('results', results.join(''));
+};
+
+generatePalindrome('hollo');
